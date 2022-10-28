62292d69a0c1541958a2e3293b17ec1409d692ea
kaneofthrones <email@email.com>
2022-10-28 14:55:31 +0100

    remove debug from app

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -52,4 +52,4 @@ def FUN_checkout():
     return render_template("checkout_error.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
